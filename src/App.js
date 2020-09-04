dbc4332bc251981745046c07cf2e3e42861069f8
Ashvinc <achellas@ucsd.edu>
2020-09-04 00:13:16 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [response, setResponse] = useState("");
+  const callAPI = async () => {
+    fetch("http://localhost:9000/testAPI").then(res => res.text()).then((text) => {setResponse(text)})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <button onClick={callAPI}>Click Me!</button>
+    <h1>{response}</h1>
     </div>
   );
 }
